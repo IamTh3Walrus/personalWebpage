5c85973d013ac9ef82a90596d80de19bfe600a91
Stephen Doe <redcard1016@yahoo.com>
2016-07-08 21:00:42 -0400

    Added Landing Page Animation

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+ $(function() {
+     $("#nameFont").typed({
+         strings: ["Stephen Doe"],
+         typeSpeed: 300
+     });
+     $("#occupationFont").typed({
+         strings: ["Web Developer -  Freelance Writer"],
+         typeSpeed: 100
+     });
+     $("#landingButton").hide();
+     $("#landingButton").toggle(1200);
+     $("#landingPageImage").hide();
+     $("#landingPageImage").toggle(1500);
+ });

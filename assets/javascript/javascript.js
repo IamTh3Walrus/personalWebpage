ff11915888de4343421d38edac1e26de4ce263f6
Stephen Doe <redcard1016@yahoo.com>
2016-08-03 20:49:28 -0400

    Added PHP

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,8 +11,4 @@
      $("#landingButton").toggle(1200);
      $("#landingPageImage").hide();
      $("#landingPageImage").toggle(1500);
-
-     $('.carousel').carousel({
-         interval: 9000
-     })
  });

8124fe2724beb7889357758a8a493858b2908ef4
Stephen Doe <redcard1016@yahoo.com>
2016-08-19 14:21:57 -0400

    Added some things that needed to be added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,4 +11,19 @@
      $("#landingButton").toggle(1200);
      $("#landingPageImage").hide();
      $("#landingPageImage").toggle(1500);
+
+     $("#aboutMe").typed({
+         strings: ["About Me"],
+         typeSpeed: 300
+     });
+     $("#contactMe").typed({
+         strings: ["Contact Me"],
+         typeSpeed: 300
+
+     });
+     $("#portfolioFont").typed({
+         strings: ["Portfolio"],
+         typeSpeed: 300
+
+     });
  });
